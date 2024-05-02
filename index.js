87ca3d962451cbfea23d4f65ff334b748a974f9b
Michelle Bell <michbesch0@outlook.com>
2024-05-02 15:20:48 -0400

    Updated index and generateMarkdown files

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your GitHub username? ',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'What is your email address? ',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is your project name? ',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project: ',
+        name: 'description'
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have? ',
+        name: 'license',
+        choices: ['MIT License', 'GNU General Public License (GPL)', 'Apache License 2.0', 'BSD 2-Clause License', 'BSD 3-Clause License', 'Mozilla Public License 2.0', 'GNU Affero General Public License (AGPL)']
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies? ',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests? ',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo? ',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo? ',
+        name: 'contributing'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log('Generating README...');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions).then((response) => {
+            writeToFile('README.md', generateMarkdown(response));
+        });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,124 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    let badge;
+    if (license !== "" && license !== null && license !== undefined) {
+        switch(license) {
+            case "MIT License":
+                badge = 'https://img.shields.io/badge/License-MIT-yellow.svg';
+                break;
+            case "GNU General Public License (GPL)":
+                badge = 'https://img.shields.io/badge/License-GPLv3-blue.svg';
+                break;
+            case "Apache License 2.0":
+                badge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
+                break;
+            case "BSD 2-Clause License":
+                badge = 'https://img.shields.io/badge/License-BSD_2--Clause-orange.svg';
+                break;
+            case "BSD 3-Clause License":
+                badge = 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg';
+                break;
+            case "Mozilla Public License 2.0":
+                badge = 'https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg';
+                break;
+            case "GNU Affero General Public License (AGPL)":
+                badge = 'https://img.shields.io/badge/License-AGPL_v3-blue.svg';
+                break;
+        }
+    } else {
+        badge = '';
+    }
+    return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    let link;
+    if (license !== "" && license !== null && license !== undefined) {
+        switch(license) {
+            case "MIT License":
+                link = 'https://opensource.org/licenses/MIT';
+                break;
+            case "GNU General Public License (GPL)":
+                link = 'https://www.gnu.org/licenses/gpl-3.0';
+                break;
+            case "Apache License 2.0":
+                link = 'https://opensource.org/licenses/Apache-2.0';
+                break;
+            case "BSD 2-Clause License":
+                link = 'https://opensource.org/licenses/BSD-2-Clause';
+                break;
+            case "BSD 3-Clause License":
+                link = 'https://opensource.org/licenses/BSD-3-Clause';
+                break;
+            case "Mozilla Public License 2.0":
+                link = 'https://opensource.org/licenses/MPL-2.0';
+                break;
+            case "GNU Affero General Public License (AGPL)":
+                link = 'https://www.gnu.org/licenses/agpl-3.0';
+                break;
+        }
+    } else {
+        link = '';
+    }
+    return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    let licenseSection;
+    if (license !== "" && license !== null && license !== undefined) {
+        licenseSection = `### ${license}`;
+    } else {
+        licenseSection = '';
+    }
+    return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `# ${data.title} 
+[![License](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Install dependencies:
+
+\`\`\`bash
+${data.install}
+\`\`\`
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+To run tests, use this command: 
+
+\`\`\`bash
+${data.tests}
+\`\`\`
+
+## Questions
+If you have any questions about the project, please feel free to visit my [GitHub](https://github.com/${data.username}) or [email me](mailto:${data.email})!
 `;
 }
 
